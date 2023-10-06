2d32b3f8f471a4adf8d263bfb6519ac27f755bac
Gabriel Amaral <gabrieldutraamaral@gmail.com>
2023-10-05 22:28:07 -0300

    Adição do ula_e_banco_tb.

--- ULA + Banco/ula_e_banco.vhd
+++ ULA + Banco/ula_e_banco.vhd
@@ -6,13 +6,14 @@ ENTITY ula_e_banco IS
     PORT (
         clk : IN STD_LOGIC;
         rst : IN STD_LOGIC;
-        enable : IN STD_LOGIC;
+        enable : IN STD_LOGIC; --> Reg_Write
         constante : IN unsigned(15 DOWNTO 0);
         ula_func : IN unsigned(1 DOWNTO 0);
+        write_data : IN unsigned(15 DOWNTO 0);
         write_register : IN unsigned(2 DOWNTO 0);
         read_register1 : IN unsigned(2 DOWNTO 0);
         read_register2 : IN unsigned(2 DOWNTO 0);
-        ALU_SrcB : IN STD_LOGIC; --determina a segunda entrada da ula
+        ALU_SrcB : IN STD_LOGIC; --> Determina a segunda entrada da ula
         ula_out : OUT unsigned(15 DOWNTO 0);
         data1 : OUT unsigned(15 DOWNTO 0);
         data2 : OUT unsigned(15 DOWNTO 0)
@@ -20,7 +21,7 @@ ENTITY ula_e_banco IS
 END ENTITY;
 
 ARCHITECTURE a_ula_e_banco OF ula_e_banco IS
-    COMPONENT banco_registradores IS
+    COMPONENT banco_de_registradores IS
         PORT (
             read_reg1 : IN unsigned(2 DOWNTO 0);
             read_reg2 : IN unsigned(2 DOWNTO 0);
@@ -36,8 +37,8 @@ ARCHITECTURE a_ula_e_banco OF ula_e_banco IS
 
     COMPONENT ula IS
         PORT (
-            entrada0 : IN unsigned(15 DOWNTO 0);
-            entrada1 : IN unsigned(15 DOWNTO 0);
+            entrada_0 : IN unsigned(15 DOWNTO 0);
+            entrada_1 : IN unsigned(15 DOWNTO 0);
             seletor_op : IN unsigned(1 DOWNTO 0);
             saida_ula : OUT unsigned(15 DOWNTO 0)
         );
@@ -54,28 +55,30 @@ ARCHITECTURE a_ula_e_banco OF ula_e_banco IS
     SIGNAL read_data1, saida_mux, mux_entr0, ula_out_internal : unsigned(15 DOWNTO 0);
 
 BEGIN
+
     ula_0 : ula PORT MAP(
-        entrada0 => read_data1,
-        entrada1 => saida_mux, --saida do mux na outra entrada da ula
+        entrada_0 => read_data1,
+        entrada_1 => saida_mux, --> Saida do mux na outra entrada da ula
         seletor_op => ula_func,
         saida_ula => ula_out_internal
     );
 
-    banco_reg_0 : banco_registradores PORT MAP(
+    banco_reg_0 : banco_de_registradores PORT MAP(
         clk => clk,
         rst => rst,
         reg_write => enable,
-        write_data => ula_out_internal, --saida da ula na entrada de dados do banco
+        write_data => write_data,
         write_reg => write_register,
         read_reg1 => read_register1,
         read_reg2 => read_register2,
-        read_data1 => read_data1, --saida do banco numa das entradas da ula
-        read_data2 => mux_entr0 --outra saida do banco num mux
+        read_data1 => read_data1, --> Saida do banco numa das entradas da ula
+        read_data2 => mux_entr0 --> Outra saida do banco num mux
     );
 
     mux_2x1_0 : mux_2x1 PORT MAP(
-        sel0 => ALU_SrcB, entr0 => mux_entr0,
-        entr1 => constante, --pino de entrada top level na outra entrada do MUX 
+        sel0 => ALU_SrcB,
+        entr0 => mux_entr0,
+        entr1 => constante, --> Pino de entrada top level na outra entrada do MUX 
         saida => saida_mux
     );
 
---
+++ ULA + Banco/ula_e_banco_tb.vhd
@@ -0,0 +1,109 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+USE ieee.numeric_std.ALL;
+
+ENTITY ula_e_banco_tb IS
+END ENTITY;
+
+ARCHITECTURE a_ula_e_banco_tb OF ula_e_banco_tb IS
+    COMPONENT ula_e_banco IS
+        PORT (
+            clk : IN STD_LOGIC;
+            rst : IN STD_LOGIC;
+            enable : IN STD_LOGIC;
+            constante : IN unsigned(15 DOWNTO 0);
+            ula_func : IN unsigned(1 DOWNTO 0);
+            write_data : IN unsigned(15 DOWNTO 0);
+            write_register : IN unsigned(2 DOWNTO 0);
+            read_register1 : IN unsigned(2 DOWNTO 0);
+            read_register2 : IN unsigned(2 DOWNTO 0);
+            ALU_SrcB : IN STD_LOGIC; --> Determina a segunda entrada da ula
+            ula_out : OUT unsigned(15 DOWNTO 0);
+            data1 : OUT unsigned(15 DOWNTO 0);
+            data2 : OUT unsigned(15 DOWNTO 0)
+        );
+    END COMPONENT;
+
+    CONSTANT period_time : TIME := 100 ns;
+    SIGNAL finished : STD_LOGIC := '0';
+    SIGNAL clk, rst, enable, Alu_SrcB : STD_LOGIC := '0';
+    SIGNAL read_reg1, read_reg2, write_reg : unsigned(2 DOWNTO 0) := "000";
+    SIGNAL sinal_op_ula : unsigned(1 DOWNTO 0) := "00";
+    SIGNAL data1, data2, write_data, constante, saida_ula : unsigned(15 DOWNTO 0) := "0000000000000000";
+
+BEGIN
+
+    utt : ula_e_banco PORT MAP(
+        clk => clk,
+        rst => rst,
+        enable => enable,
+        constante => constante,
+        ula_func => sinal_op_ula,
+        write_data => write_data,
+        write_register => write_reg,
+        read_register1 => read_reg1,
+        read_register2 => read_reg2,
+        ALU_SrcB => Alu_SrcB, --> Determina a segunda entrada da ula
+        ula_out => saida_ula,
+        data1 => data1,
+        data2 => data2
+
+    );
+
+    rst_global : PROCESS
+    BEGIN
+        rst <= '1';
+        WAIT FOR period_time;
+        rst <= '0';
+        WAIT;
+    END PROCESS;
+
+    sim_time_proc : PROCESS
+    BEGIN
+        WAIT FOR period_time * 10;
+        finished <= '1';
+        WAIT;
+    END PROCESS sim_time_proc;
+
+    clk_proc : PROCESS
+    BEGIN
+        WHILE finished /= '1' LOOP
+            clk <= '0';
+            WAIT FOR period_time/2;
+            clk <= '1';
+            WAIT FOR period_time/2;
+        END LOOP;
+        WAIT;
+    END PROCESS clk_proc;
+
+    PROCESS
+    BEGIN
+        WAIT FOR 100 ns; --> Reset
+
+        write_reg <= "000";
+        enable <= '1';
+        write_data <= "0000000000000011";
+        WAIT FOR 100 ns;
+
+        enable <= '0';
+        WAIT FOR 100 ns;
+
+        write_reg <= "001";
+        enable <= '1';
+        write_data <= "0000000000001111";
+        WAIT FOR 100 ns;
+
+        enable <= '0';
+        WAIT FOR 100 ns;
+
+        read_reg1 <= "000";
+        read_reg2 <= "001";
+        WAIT FOR 100 ns;
+
+        sinal_op_ula <= "10";
+        Alu_SrcB <= '0';
+        WAIT FOR 100 ns;
+        WAIT;
+    END PROCESS;
+
+END ARCHITECTURE;
